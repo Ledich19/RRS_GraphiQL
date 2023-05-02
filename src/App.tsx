e52f388d7edc9a8778f7554940142e9d6558c1b1
NRG-Spirit <102809707+NRG-Spirit@users.noreply.github.com>
2023-05-02 16:13:06 +0300

    feat: add footer without styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { Outlet, Routes, Route, Navigate } from 'react-router-dom';
 import ErrorPage from './pages/Page404/Page404';
 import Layout from './pages/Layout/Layout';
 import AboutUs from './pages/AboutUs/AboutUs';
+import Footer from './componemts/Footer/Footer';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="*" element={<Layout />}>
-        <Route index element={<AboutUs />} />
-        <Route path="about" element={<AboutUs />} />
-        <Route path="404" element={<ErrorPage />} />
-        <Route path="*" element={<Navigate to="404" />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="*" element={<Layout />}>
+          <Route index element={<AboutUs />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="404" element={<ErrorPage />} />
+          <Route path="*" element={<Navigate to="404" />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </>
   );
 };
 
