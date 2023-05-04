b303c8a7a2c78f43f4056a9c2e2071d525d984b7
pedanmax <m.pedan2017@gmail.com>
2023-05-04 19:02:17 +0300

    feat:add semantic tags to header and basic layout header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,35 @@
-import { NavLink } from 'react-router-dom';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../app/firebase';
+import Authorization from '../AuthorizationBtnBlock/AuthorizationBtnBlock';
 import s from './Header.module.scss';
 
 const Header: React.FC = () => {
+  const [user, loading, error] = useAuthState(auth);
   return (
-    <div className={s.header}>
-      <NavLink to="/" className={(info) => (info.isActive ? s.activeLink : s.navLink)}>
-        Home
-      </NavLink>
-      <NavLink to="/app" className={(info) => (info.isActive ? s.activeLink : s.navLink)}>
-        About us
-      </NavLink>
-    </div>
+    <header className={s.header}>
+      <div className={s.header__container}>
+        <nav className={s.header__navigation}>
+          <ul className={s.header__list}>
+            <li className={s.header__item}>
+              <NavLink to="/" className={(info) => (info.isActive ? s.activeLink : s.navLink)}>
+                Home
+              </NavLink>
+            </li>
+            <li className={s.header__item}>
+              <NavLink to="/app" className={(info) => (info.isActive ? s.activeLink : s.navLink)}>
+                Editor
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className={s.header__lang}>language: en</div>
+        <div className={s.header__user}>
+          <Authorization isUser={user} />
+        </div>
+      </div>
+    </header>
   );
 };
 
