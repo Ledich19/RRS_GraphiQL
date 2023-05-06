ff2d85e4a37b6c65bb51d5d7f3fc0d3094090fc7
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-06 18:02:51 +0300

    feat: add validation

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,1 +1,9 @@
 export type NotifyType = { type: string | null; text: string | null };
+export type ValidationType = {
+  isEmpty: boolean;
+  minLength: number;
+  reGex?: {
+    value: RegExp;
+    text: string;
+  };
+};
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,10 +4,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, logInWithEmailAndPassword, signInWithGoogle } from '../../app/firebase';
 import s from './Login.module.scss';
 import LinkBtn from '../LinkBtn/LinkBtn';
+import useInput from '../../hooks/useInput';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  const email = useInput();
+  const password = useInput();
 
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
@@ -26,21 +29,23 @@ const Login = () => {
       <input
         type="text"
         className={s.email}
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={email.value}
+        onChange={(e) => email.onChange(e)}
+        onBlur={(e) => email.onBlur(e)}
         placeholder="E-mail Address"
       />
       <input
         type="password"
         className={s.password}
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        value={password.value}
+        onChange={(e) => password.onChange(e)}
+        onBlur={(e) => password.onBlur(e)}
         placeholder="Password"
       />
       <button
         type="button"
         className={s.loginBtn}
-        onClick={() => logInWithEmailAndPassword(email, password)}
+        onClick={() => logInWithEmailAndPassword(email.value || '', password.value || '')}
       >
         Login
       </button>
