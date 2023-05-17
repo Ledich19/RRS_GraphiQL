0657b0ec8edfed663853b7cf76abbf9afa1dab2d
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-17 20:26:20 +0300

    style: add footer style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import style from './Footer.module.scss';
+import logoRed from '../../assets/logo/logo-git-red.png';
+import logoGreen from '../../assets/logo/logo-git-green.png';
+import logoBlue from '../../assets/logo/logo-git-blue.png';
 
 const Footer: React.FC = () => {
   return (
@@ -8,16 +11,16 @@ const Footer: React.FC = () => {
       <Link to="https://rs.school/" className={style.rsschool} />
       <span className={style.year}>2023</span>
       <div className={style.authors}>
-        <Link to="https://github.com/ledich19" className={style.author}>
-          <img src="./img/logo-git.png" alt="git-logo" className={style.author_logo} />
+        <Link to="https://github.com/ledich19" className={style.author1}>
+          <img src={logoRed} alt="git-logo" className={style.logo} />
           ledich19
         </Link>
-        <Link to="https://github.com/pedanmax" className={style.author}>
-          <img src="./img/logo-git.png" alt="git-logo" className={style.author_logo} />
+        <Link to="https://github.com/pedanmax" className={style.author2}>
+          <img src={logoBlue} alt="git-logo" className={style.logo} />
           pedanmax
         </Link>
-        <Link to="https://github.com/NRG-Spirit" className={style.author}>
-          <img src="./img/logo-git.png" alt="git-logo" className={style.author_logo} />
+        <Link to="https://github.com/NRG-Spirit" className={style.author3}>
+          <img src={logoGreen} alt="git-logo" className={style.logo} />
           nrg-spirit
         </Link>
       </div>
