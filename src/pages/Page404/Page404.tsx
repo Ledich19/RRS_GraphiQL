c9d707848755504d3e8a12c57ee22449ad848e22
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-27 16:25:27 +0300

    style: sdd 404 style

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -3,8 +3,10 @@ import s from './Page404.module.scss';
 const ErrorPage = () => {
   return (
     <div className={s.error}>
-      <h1>404</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <div className={s.box}>
+      <h1 className={s.title}>404</h1>
+      <p className={s.text}>Sorry, an unexpected error has occurred.</p>
+      </div>
     </div>
   );
 };
